8ebdf9d6b8baf4eee3dea217d63bf36fb1127355
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-14 20:17:19 +0530

    Add mvc based express practise shopping-cart-app

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controller/products');
+
+router.get('/add-product',productsController.getAddProducts)
+ router.post('/add-product', productsController.postAddProducts);
+
+
+module.exports = router;
